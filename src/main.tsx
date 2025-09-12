49b3d6054eb39d35edef8a2e7d2d1ca619a90deb
Harrison Chen <wharrison832@gmail.com>
2025-09-12 23:17:51 +0800

    1

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,12 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { Toaster } from 'sonner';
 import App from "./App.tsx";
+import { baseName } from "./router";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={baseName}>
       <App />
       <Toaster />
     </BrowserRouter>
