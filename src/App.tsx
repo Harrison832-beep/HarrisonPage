f4a02d2b69aa25731404df0ceecf370c05e41ac5
Harrison Chen <wharrison832@gmail.com>
2025-09-13 09:31:24 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "@/pages/Home";
-import { useState } from "react";
-import { AuthContext } from '@/contexts/authContext';
+import { baseName } from "./router";
 
 export default function App() {
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
-
-  const logout = () => {
-    setIsAuthenticated(false);
-  };
 
   return (
-    <AuthContext.Provider
-      value={{ isAuthenticated, setIsAuthenticated, logout }}
-    >
+    <div>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/other" element={<div className="text-center text-xl">Other Page - Coming Soon</div>} />
+        <Route path={`/${baseName}`} element={<Home />} />
       </Routes>
-    </AuthContext.Provider>
+    </div>
   );
 }
